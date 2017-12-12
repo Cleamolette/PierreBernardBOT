f2a6ac515582bc0e3625d06380419ff078f4f4e5
Yenaman <33003685+Yenaman@users.noreply.github.com>
2017-12-12 12:12:16 +0100

    Add files via upload

--- zouiz.js
+++ zouiz.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var http = require("http");
+setInterval(function() {
+    http.get("<url>");
+}, 300000); 
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
