3d42d61551890ffb3ae7009c781a2842f91f7221
virginie bourcy <yenamanwow@gmail.com>
2017-12-13 22:51:56 +0100

    fix

--- zouiz.js
+++ zouiz.js
@@ -28,7 +28,7 @@ bot.on("ready", () => {
 	bot.user.setGame("^^");
 	//bot.user.setUsername('Pierre Bernard')
 	//bot.user.setAvatar("./Nagui.JPG")
-	bot.status('dnd')
+	bot.user.status('dnd')
 });
 
 bot.on('message', message => {
