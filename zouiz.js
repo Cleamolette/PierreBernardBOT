1527a4fe046d0d39b23117842928a54ffd8ec1f5
virginie bourcy <yenamanwow@gmail.com>
2017-12-14 18:36:16 +0100

    Fix ^^

--- zouiz.js
+++ zouiz.js
@@ -41,6 +41,7 @@ bot.on('message', message => {
 	}
 	if(message.content.includes("😡")) {
 		message.channel.send("JE SUIS UN HOMME EN COLÈRE ! https://yenaman.s-ul.eu/J3RY8Io5")
+		message.delete(800);
 		console.log("Quelqu'un est un homme en COLÈRE")
 	}
 	if(message.content === "*Hello darkness my old friend*") {
