e9a4f607ddd55e28ee3cf13dd9c66b7364b8548d
Yenaman <yenamanwow@gmail.com>
2017-12-16 22:04:30 +0100

    fix

--- zouiz.js
+++ zouiz.js
@@ -329,7 +329,7 @@ bot.on('message', message => {
 		message.channel.send('Ping du bot en cours...').then(sent => {
 			sent.edit(`**Ping :** \`${sent.createdTimestamp - message.createdTimestamp} ms\``);
 		});
-		var date = new Date().toCETstring()
+		var date = new Date().toLocaleTimeString()
 		bot.channels.get("391601508158013440").send(`${date} >> Commande !ping`)
 	}
 	if(message.content === "!spam") { //Embed !spam
