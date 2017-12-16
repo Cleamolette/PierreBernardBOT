86cb63c0a75784efb76e793d4800af1be43d4986
virginie bourcy <yenamanwow@gmail.com>
2017-12-16 21:40:09 +0100

    fix

--- zouiz.js
+++ zouiz.js
@@ -329,7 +329,7 @@ bot.on('message', message => {
 		message.channel.send('Ping du bot en cours...').then(sent => {
 			sent.edit(`**Ping :** \`${sent.createdTimestamp - message.createdTimestamp} ms\``);
 		});
-		bot.channels.get("391601508158013440").send(`${message.createdTimestamp} >> Commande ``!ping```)
+		bot.channels.get("391601508158013440").send(`${message.createdTimestamp} >> Commande !ping`)
 	}
 	if(message.content === "!spam") { //Embed !spam
 		const embed = {
