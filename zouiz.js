859bc3754a6f9b8014f1149057a2ee3055ea5a39
Yenaman <yenamanwow@gmail.com>
2017-12-20 20:34:48 +0100

    Ajout !rolelist

--- zouiz.js
+++ zouiz.js
@@ -504,6 +504,35 @@ bot.on('message', message => {
 		let member = message.member;
 		member.removeRole(role).catch(console.error);
 	}
+	if(message.content === "!rolelist") {
+		const embed = {
+			"title": "Liste des rôles attribuables",
+			"description": "Voici une liste des rôles que vous pouvez vous attribuer en faisant ``!role+`` pour l'ajouter ou ``!role-`` pour le retirer.",
+			"color": 8764472,
+			"thumbnail": {
+			  "url": "https://canary.discordapp.com/assets/ffcdb50ce310bfbe221f01a8e72034a8.svg"
+			},
+			"fields": [
+			  {
+				"name": "Garde de Hurlevent",
+				"value": "``!role+ Garde de Hurlevent`` ou ``!role- Garde de Hurlevent``"
+			  },
+			  {
+				"name": "Crocs de Wrynn",
+				"value": "``!role+ Crocs de Wrynn`` ou ``!role- Crocs de Wrynn``"
+			  },
+			  {
+				"name": "Main de Lordaeron",
+				"value": "``!role+ Main de Lordaeron`` ou ``!role- Main de Lordaeron``"
+			  },
+			  {
+				"name": "<:attention:314418019130671106> ATTENTION ! <:attention:314418019130671106>",
+				"value": "Tout membre retrouvé avec un grade ne lui appartenant pas subira des sanctions de la part du Conseil des neuf."
+			  }
+			]
+		  };
+		  message.channel.send({embed});
+	}
 });
 
 bot.login(process.env.TOKEN);
