1b150ace89f87e7424dce41a9ca502b4fb012508
Yenaman <yenamanwow@gmail.com>
2017-12-24 18:01:19 +0100

    Fix

--- commands/changelog.js
+++ commands/changelog.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, message, args) => {
     const embed = {
         "title": "Changelog / Nouveautés",
-        "description": "Mimiron BOT **v1.183**",
+        "description": "Mimiron BOT **v1.184**",
         "color": 5560853,
         "timestamp": "2017-12-24T16:55:00.000Z",
         "footer": {
@@ -17,7 +17,7 @@ module.exports.run = async (bot, message, args) => {
           },
           {
             "name": "[INTERNE] Dossier ./commands/",
-            "value": "Pour ceux ayant l'envie de regarder le code du bot sur GitHub :\| Il existe à présent un dossier ``commands`` pour alléger le nombre de lignes du fichier principal."
+            "value": "Pour ceux ayant l'envie de regarder le code du bot sur GitHub :\nIl existe à présent un dossier ``commands`` pour alléger le nombre de lignes du fichier principal."
           }
         ]
       };
