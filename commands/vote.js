5adca73bada220dc0709e94d0139dabc1eb64f7e
Yenaman <yenamanwow@gmail.com>
2017-12-27 00:37:40 +0100

    test fix

--- commands/vote.js
+++ commands/vote.js
@@ -1,5 +1,5 @@
-const agree = "GreenTick";
-const disagree = "RedTick";
+const agree = "395357069051691018";
+const disagree = "395357068976062475";
 
 module.exports.run = async (bot, message, args) => {
     let msg = await message.channel.send("Votez !");
