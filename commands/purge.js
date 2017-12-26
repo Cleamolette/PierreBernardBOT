44abac4e6b0d73d63610dfeb86f49fe36c1f34cb
Yenaman <yenamanwow@gmail.com>
2017-12-26 17:31:56 +0100

    fix

--- commands/purge.js
+++ commands/purge.js
@@ -4,7 +4,9 @@ module.exports.run = async (bot, message, args) => {
         if(!message.member.roles.find("name", "Le Conseil")) {
             message.channel.send(`Vous n'avez pas la permission d'utiliser cette commande.`);
             return;
-        if(!message.author.id === '267357209187450884') return;
+        if(!message.member.roles.find("name", "7ARG")) {
+            message.channel.send(`Vous n'avez pas la permission d'utiliser cette commande.`);
+        }
         }
         if(isNaN(args[0])) {
             message.channel.send(`Syntaxe de la commande :\n \`!purge <nombre de messages à supprimer>\``);
@@ -12,8 +14,8 @@ module.exports.run = async (bot, message, args) => {
         }
         const fetched = await message.channel.fetchMessages({limit: args[0]});
         console.log(fetched.size + ' messages supprimés ✅');
-        bot.channels.get("389843676966158348").send(fetched.size + ' messages supprimés :check:')
-        bot.channels.get("391601508158013440").send(fetched.size + ' messages supprimés :check:')
+        bot.channels.get("389843676966158348").send(fetched.size + ' messages supprimés <:check:395246034575425537>')
+        bot.channels.get("391601508158013440").send(fetched.size + ' messages supprimés <:check:395246034575425537>')
         message.channel.bulkDelete(fetched)
             .catch(error => message.channel.send(`Erreur : ${error}`));
     }
