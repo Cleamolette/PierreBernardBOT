8c27a13374d82bfb4aac7711bf00b38e220b709e
Yenaman <yenamanwow@gmail.com>
2017-12-26 22:05:59 +0100

    test fix

--- zouiz.js
+++ zouiz.js
@@ -387,7 +387,7 @@ bot.on('message', message => {
 	}
 });
 
-client.on('guildMemberAdd', member => {
+bot.on('guildMemberAdd', member => {
 	const channel = member.guild.channels.find('name', 'member-log');
 	if (!channel) return;
 	channel.send(`Welcome to the server, ${member}`);
