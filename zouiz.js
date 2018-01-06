1e70720126a6ced1e28607f73f5fb301fdac9cbc
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-01-06 23:56:00 +0100

    fix date homme en colère

--- zouiz.js
+++ zouiz.js
@@ -63,6 +63,7 @@ bot.on('message', message => {
 //	}
 	if(message.content.includes("😡")) {
 		message.channel.send("JE SUIS UN HOMME EN COLÈRE ! https://yenaman.s-ul.eu/J3RY8Io5")
+		var date = new Date().toLocaleTimeString()
 		bot.channels.get("391601508158013440").send(`${date}(GMT+0) >> Commande Homme en colère`)
 		console.log("Quelqu'un est un homme en COLÈRE")
 	}
