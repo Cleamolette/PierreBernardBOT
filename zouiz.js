39d5d78005ad658ab84d8e458cca69acc11fe361
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-01-07 00:38:40 +0100

    Update zouiz.js

--- zouiz.js
+++ zouiz.js
@@ -383,9 +383,6 @@ bot.on('message', message => {
 	if(message.content === "``Rôle retiré`` :white_check_mark:") {
 		message.delete(5000)
 	}
-	if(message.content.includes("Un redémarrage du bot a été lancé.")) {
-		process.exit(1);
-	}
 });
 
 bot.login(process.env.TOKEN);
