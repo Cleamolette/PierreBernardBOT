b298cb03196e4667642b425ff455d3f648686d66
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-01-07 00:44:09 +0100

    Update config.js

--- commands/config.js
+++ commands/config.js
@@ -29,9 +29,9 @@ if(message.content.includes("restart")) {
         "color": 16711680
       };
       message.channel.send({embed});
+      message.channel.send("Un redémarrage du bot a été lancé.")
       var date = new Date().toLocaleTimeString()
       bot.channels.get("391601508158013440").send(`${date}(GMT+0) >> Commande !config **restart**`)
-        console.log(`>> Commande !config restart`)
-    process.exit(0)
+        console.log(`>> Commande !config restart`).then(process.exit(0));
 }
 }
