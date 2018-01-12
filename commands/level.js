3632ceeffc5503e11239193532b8ee3cc7aa4630
Yenaman <yenamanwow@gmail.com>
2018-01-12 17:14:37 +0100

    Test fix

--- commands/level.js
+++ commands/level.js
@@ -3,8 +3,8 @@ module.exports.run = async (bot, message, args) => {
   }
   const fs = require("fs");
   let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
-  let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
   let userData = points[message.author.id];
+  let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
   fs.writeFile("./points.json", JSON.stringify(points), (err) => {
     if (err) console.error(err)
   });
