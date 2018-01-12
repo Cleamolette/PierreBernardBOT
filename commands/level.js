8c952745b875b6a5a27db08c7cd11550e64f2a0f
Yenaman <yenamanwow@gmail.com>
2018-01-12 17:11:07 +0100

    Test

--- commands/level.js
+++ commands/level.js
@@ -1,6 +1,7 @@
 module.exports.run = async (bot, message, args) => {
     message.reply(`You are currently level ${userData.level}, with ${userData.points} points.`);
   }
+  const fs = require("fs");
   fs.writeFile("./points.json", JSON.stringify(points), (err) => {
     if (err) console.error(err)
   });
