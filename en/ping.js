6fdcdfcf77ab2bae210abf20c6fb56523b958ab3
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-01-14 02:30:44 +0100

    New translations ping.js (English)

--- en/ping.js
+++ en/ping.js
@@ -1,8 +1,8 @@
 module.exports.run = async (bot, message, args) => {
-    message.channel.send('Ping du bot en cours...').then(sent => {
+    message.channel.send('Pinging the bot...').then(sent => {
         sent.edit(`**Ping :** \`${sent.createdTimestamp - message.createdTimestamp} ms\``);
     });
     var date = new Date().toLocaleTimeString()
-    bot.channels.get("391601508158013440").send(`${date}(GMT+0) >> Commande !ping`)
-        console.log(`>> Commande !ping`)
+    bot.channels.get("391601508158013440").send(`${date}(GMT+0) >> Command !ping`)
+        console.log(`>> Command !ping`)
 }
