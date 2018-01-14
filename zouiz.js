99f639469bdfae06203dec70f54486241a79b779
Yenaman <yenamanwow@gmail.com>
2018-01-14 20:57:57 +0100

    activity

--- zouiz.js
+++ zouiz.js
@@ -45,7 +45,7 @@ bot.on('message', message => {
 });
 
 bot.on("ready", () => {
-	bot.user.setGame("!info");
+	bot.user.setActivity("Jacques Cheminade", LISTENING);
 	//bot.user.setUsername('Mimiron')
 	//bot.user.setAvatar("./Mimiron.jpg")
 	console.log("MimironBOT : Opérationnel 👍");
