bf6925ffd687980a2deced62faf522f2ec8abf5a
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-01-14 02:30:43 +0100

    New translations membres.js (English)

--- en/membres.js
+++ en/membres.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, message, args) => {
-    message.channel.send(`\`Il y a ${message.guild.memberCount} membres dans le serveur\``);
+    message.channel.send(`\`There are ${message.guild.memberCount} members in the server\``);
     var date = new Date().toLocaleTimeString()
-    bot.channels.get("391601508158013440").send(`${date}(GMT+0) >> Commande !membres`)
-    console.log(`>> Commande !membres`)
+    bot.channels.get("391601508158013440").send(`${date}(GMT+0) >> Command !members`)
+    console.log(`>> Command !members`)
 }
