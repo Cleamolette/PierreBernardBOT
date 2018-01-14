41b9aa3af4dbc1c6b3d3e685f08079df7bd3250b
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-01-14 15:22:18 +0100

    New translations suggestion.js (English)

--- en/suggestion.js
+++ en/suggestion.js
@@ -2,6 +2,6 @@ module.exports.run = async (bot, message, args) => {
     let text = args.slice(0).join(" ");
     let author = message.author;
     message.delete(2000)
-    message.channel.send("Suggestion envoyée ! :tada:")
-    bot.channels.get("395362240523665408").send(`<@267357209187450884> Nouvelle suggestion !\n\n\`${text}\`\nSuggestion de ${author}`);
+    message.channel.send("Suggestion sent! :tada:")
+    bot.channels.get("395362240523665408").send(`<@267357209187450884> New suggestion !\n\n\`${text}\`\nBy ${author}`);
 }
