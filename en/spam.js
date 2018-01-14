7481be3219b97c080f57e08b8cebea3a9be89ab6
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-01-14 01:58:04 +0100

    New translations spam.js (English)

---
+++ en/spam.js
@@ -0,0 +1,23 @@
+module.exports.run = async (bot, message, args) => {
+        const embed = {
+            "title": "Rappel des règles quant au *spam*",
+            "color": 15472173,
+            "thumbnail": {
+              "url": "https://cdn.discordapp.com/emojis/332985690075693059.png"
+            },
+            "fields": [
+            {
+            "name": "Règle 2",
+            "value": "Pas d'insultes/**Spam**/**Flood** ni racisme, homophobie"
+            },
+            {
+            "name": "Règle 5",
+            "value": "Conversations bordel nawak : <#388170884722196490> ou <#388844665111183360>. **Sanction : Rétrogradation**"
+            }
+            ]
+        };
+        message.channel.send({embed});
+        var date = new Date().toLocaleTimeString()
+        bot.channels.get("391601508158013440").send(`${date}(GMT+0) >> Commande !spam`)
+        console.log(`>> Commande !spam`)
+    }
