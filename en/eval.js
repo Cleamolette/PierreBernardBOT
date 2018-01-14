08288bc5bc42e348826a6f419c75a673709525ee
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-01-14 01:58:13 +0100

    New translations eval.js (English)

---
+++ en/eval.js
@@ -0,0 +1,20 @@
+module.exports.run = async (bot, message, args) => {
+  if (message.content.startsWith("!eval")) {
+    if(message.author.id !== '267357209187450884') return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
+}
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+  }
