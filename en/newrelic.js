b5675a3f10bdb87b56cba0a24ed95a500139814d
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-01-14 01:58:03 +0100

    New translations newrelic.js (English)

---
+++ en/newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config/default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['MimironBOT'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '234379fb9702d5dd8a54defe0cc53c129d5e0392',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
