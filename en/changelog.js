850f251de79c2f58bf2feb2939a9f70434660b98
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-01-14 02:11:19 +0100

    New translations changelog.js (English)

--- en/changelog.js
+++ en/changelog.js
@@ -1,11 +1,11 @@
 module.exports.run = async (bot, message, args) => {
     const embed = {
-        "title": "Changelog / Nouveautés",
+        "title": "Changelog",
         "description": "Mimiron BOT **v1.2.82**",
         "color": 5560853,
         "timestamp": "2018-01-13T16:17:00.000Z",
         "footer": {
-          "text": "Dernière mise à jour le"
+          "text": "Last update"
         },
         "thumbnail": {
           "url": "https://cdn.discordapp.com/avatars/272416422444007424/c5f309543416a382339859addb4af97e.png"
@@ -13,16 +13,16 @@ module.exports.run = async (bot, message, args) => {
         "fields": [
           {
             "name": "!rolelist / !role- / !role+",
-            "value": `Mise à jour de la liste.`
+            value": `List update`
           },
           {
             "name": "!help",
-            "value": `Mise à jour de la liste.`
+            value": `List update`
           }
         ]
       };
       message.channel.send({embed});
       var date = new Date().toLocaleTimeString()
-      bot.channels.get("391601508158013440").send(`${date}(GMT+0) >> Commande !changelog`)
-        console.log(`>> Commande !changelog`)
+      bot.channels.get("391601508158013440").send(`${date}(GMT+0) >> Command !changelog`)
+        console.log(`>> Command !changelog`)
 }
