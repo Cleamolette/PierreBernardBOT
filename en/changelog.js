5ad4ffb485f1483af88e1cf0e825381dc59cc2d9
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-01-21 17:41:06 +0100

    New translations changelog.js (English)

--- en/changelog.js
+++ en/changelog.js
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
         "fields": [
           {
             "name": "Counting",
-            "value": `Now you can count in a dedicated channel!`
+            "value": `Vous pouvez maintenant compter dans un channel prévu à cet effet !`
           }
         ]
       };
