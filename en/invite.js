cb4dcd96e2b856f2e4fded8ec05d307d5744c8a1
Yenaman <33003685+Yenaman@users.noreply.github.com>
2018-02-04 00:01:28 +0100

    New translations invite.js (English)

--- en/invite.js
+++ en/invite.js
@@ -1,27 +1,117 @@
 module.exports.run = async (bot, message, args) => {
-    if(message.content === "!invite 1") {
+    if(message.content === "!invite 1 1") {
         bot.channels.get("409456956466659330").createInvite({maxAge: 0, maxUses: 1})
-        .then(invite => message.channel.send(`1 invitation à ce channel : discord.gg/${invite.code}`))
+        .then(invite => message.channel.send(`1 invitation à ce channel : http://discord.gg/${invite.code}`))
         .catch(console.error);
     }
-    if(message.content === "!invite 5") {
-        message.channel.createInvite({maxAge: 0, maxUses: 5})
-        .then(invite => message.channel.send(`5 invitations à ce channel : discord.gg/${invite.code}`))
+    if(message.content === "!invite 1 5") {
+        bot.channels.get("409456956466659330").createInvite({maxAge: 0, maxUses: 5})
+        .then(invite => message.channel.send(`5 invitations à ce channel : http://discord.gg/${invite.code}`))
         .catch(console.error);
     }
-    if(message.content === "!invite 10") {
-        message.channel.createInvite({maxAge: 0, maxUses: 10})
-        .then(invite => message.channel.send(`10 invitations à ce channel : discord.gg/${invite.code}`))
+    if(message.content === "!invite 1 10") {
+        bot.channels.get("409456956466659330").createInvite({maxAge: 0, maxUses: 10})
+        .then(invite => message.channel.send(`10 invitations à ce channel : http://discord.gg/${invite.code}`))
         .catch(console.error);
     }
-    if(message.content === "!invite 25") {
-        message.channel.createInvite({maxAge: 0, maxUses: 25})
-        .then(invite => message.channel.send(`25 invitations à ce channel : discord.gg/${invite.code}`))
+    if(message.content === "!invite 1 25") {
+        bot.channels.get("409456956466659330").createInvite({maxAge: 0, maxUses: 25})
+        .then(invite => message.channel.send(`25 invitations à ce channel : http://discord.gg/${invite.code}`))
         .catch(console.error);
     }
-    if(message.content === "!invite 30") {
-        message.channel.createInvite({maxAge: 0, maxUses: 30})
-        .then(invite => message.channel.send(`30 invitations à ce channel : discord.gg/${invite.code}`))
+    if(message.content === "!invite 1 30") {
+        bot.channels.get("409456956466659330").createInvite({maxAge: 0, maxUses: 30})
+        .then(invite => message.channel.send(`30 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 1 40") {
+        bot.channels.get("409456956466659330").createInvite({maxAge: 0, maxUses: 40})
+        .then(invite => message.channel.send(`40 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 1 50") {
+        bot.channels.get("409456956466659330").createInvite({maxAge: 0, maxUses: 50})
+        .then(invite => message.channel.send(`50 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 2 1") {
+        bot.channels.get("409456976767090703").createInvite({maxAge: 0, maxUses: 1})
+        .then(invite => message.channel.send(`1 invitation à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 2 5") {
+        bot.channels.get("409456976767090703").createInvite({maxAge: 0, maxUses: 5})
+        .then(invite => message.channel.send(`5 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 2 10") {
+        bot.channels.get("409456976767090703").createInvite({maxAge: 0, maxUses: 10})
+        .then(invite => message.channel.send(`10 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 2 25") {
+        bot.channels.get("409456976767090703").createInvite({maxAge: 0, maxUses: 25})
+        .then(invite => message.channel.send(`25 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 2 30") {
+        bot.channels.get("409456976767090703").createInvite({maxAge: 0, maxUses: 30})
+        .then(invite => message.channel.send(`30 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 3 1") {
+        bot.channels.get("409456997679890435").createInvite({maxAge: 0, maxUses: 1})
+        .then(invite => message.channel.send(`1 invitation à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 3 5") {
+        bot.channels.get("409456997679890435").createInvite({maxAge: 0, maxUses: 5})
+        .then(invite => message.channel.send(`5 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 3 10") {
+        bot.channels.get("409456997679890435").createInvite({maxAge: 0, maxUses: 10})
+        .then(invite => message.channel.send(`10 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 3 25") {
+        bot.channels.get("409456997679890435").createInvite({maxAge: 0, maxUses: 25})
+        .then(invite => message.channel.send(`25 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 3 30") {
+        bot.channels.get("409456997679890435").createInvite({maxAge: 0, maxUses: 30})
+        .then(invite => message.channel.send(`30 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 4 1") {
+        bot.channels.get("409457023470534666").createInvite({maxAge: 0, maxUses: 1})
+        .then(invite => message.channel.send(`1 invitation à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 4 5") {
+        bot.channels.get("409457023470534666").createInvite({maxAge: 0, maxUses: 5})
+        .then(invite => message.channel.send(`5 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 4 10") {
+        bot.channels.get("409457023470534666").createInvite({maxAge: 0, maxUses: 10})
+        .then(invite => message.channel.send(`10 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 4 25") {
+        bot.channels.get("409457023470534666").createInvite({maxAge: 0, maxUses: 25})
+        .then(invite => message.channel.send(`25 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 4 30") {
+        bot.channels.get("409457023470534666").createInvite({maxAge: 0, maxUses: 30})
+        .then(invite => message.channel.send(`30 invitations à ce channel : http://discord.gg/${invite.code}`))
+        .catch(console.error);
+    }
+    if(message.content === "!invite 5 1") {
+        bot.channels.get("409457205226504225").createInvite({maxAge: 0, maxUses: 1})
+        .then(invite => message.channel.send(`1 invitation à ce channel : http://discord.gg/${invite.code}`))
         .catch(console.error);
     }
 }
